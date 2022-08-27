2639bc8e01f9bcaa56a8ed22247ea5071468122e
Julio Garrido Vico <juliogarridovico@gmail.com>
2022-08-27 12:05:37 +0200

    test

---
+++ practicaW8D3.vue
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html Lang="es">
+<head>
+	<meta charset="UTF-8"></meta>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Practica semana 8: Hola mundo</title>
+</head>
+<body>
+	<div>
+		<h1></h1>
+	</div>
+</body>
+</html>
