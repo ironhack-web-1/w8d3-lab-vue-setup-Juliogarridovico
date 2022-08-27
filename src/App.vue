b99a9acb51c5d6adc1f72539f8e6bd37bb9ef1d9
Julio Garrido Vico <juliogarridovico@gmail.com>
2022-08-27 13:50:15 +0200

    test git commit -m test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>Vue 3!</div>
+  <AboutComponent />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import AboutComponent from "./components/About.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AboutComponent,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>{{ aboutMe }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "AboutComponent",
+  data: () => ({
+    aboutMe: "lorem ipsum",
+  }),
+};
+</script>
